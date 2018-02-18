ad12920007c5f451ec0e992d09328814f1ee425e
emmyavalos <33555031+emmyavalos@users.noreply.github.com>
2018-02-18 14:17:34 -0800

    Update contact_list.rb

--- contact_list.rb
+++ contact_list.rb
@@ -1,65 +1,72 @@
 require "pry"
 require 'colorize'
 
-contacts = []
 
-contact_details {}
+contacts = [{name: "emmy", email: "emmy@fakemail.com", phone: "4158675309"},]
 
-def view_contacts
-  contacts.each { |key,value| puts "#{key}: #{value}" }
-end
+# def view_contacts
+#   contacts.each { |key,value| puts "#{key}: #{value}" }
+# end
+# end
 
 
 def new_contact
+  contact_hash = {}
   puts "Enter Contact Name:"
   name = gets.strip
+  contact_hash[:name] = name
    # need error checking for each
-    name.length > 0 ? contact_details << name : "Please enter a valid name:"
+    # name.length > 0 ? contact_details << name : "Please enter a valid name:"
   puts "Enter Contact Email"
   email = gets.strip
   puts "Enter Contact Phone"
 end
 
 def edit_contact
-
 end
 
 def delete_contact
-
+  #display contact each with index, delete using index
 end
 
 
 
-# print user options to screen
-puts "~~Contacts List Menu~~"
-puts "1) View all Contacts"
-puts "2) Add a New Contact"
-puts "3) Edit a Contact"
-puts "4) Delete A Contact"
-puts "5) Exit "
-puts "\n"
+def menu
+  puts "~~Contacts List Menu~~"
+  puts "1) View all Contacts"
+  puts "2) Add a New Contact"
+  puts "3) Edit a Contact"
+  puts "4) Delete A Contact"
+  puts "5) Exit "
+  puts "\n"
+end
 
 choice = gets.chomp.to_i
 
+while true
 
-case choice
-  when 1
-    view_contacts
+  case choice
+    when 1
+      view_contacts
 
-  when 2
-    new_contact
+    when 2
+      new_contact
 
-  when 3
-    edit_contact
+    when 3
+      edit_contact
 
-  when 4
-    delete_contact
+    when 4
+      delete_contact
 
-  when 5
-    puts "Thank you for using the contact list"
-    exit(0)
-  else
-    puts "That's not a valid option, please try again."
+    when 5
+      puts "Thank you for using the contact list"
+      exit(0)
+    else
+      puts "That's not a valid option, please try again."
+    end
   end
+end
 
 end
+
+menu
